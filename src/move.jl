bd5d2bb02f0baaaa446a0edfb0bff89ee3fa86ec
edwinb-ai <developeredwin@gmail.com>
2020-02-24 17:50:59 -0600

    Added some comments and using the system dimensionality information for the RNG creation

--- src/move.jl
+++ src/move.jl
@@ -8,23 +8,27 @@ function _move_loop!(N::Integer, s::SimulationSystem, pot::PairwisePotential)
     # Check first energy
     total_energy = zero(s.energy)
     total_energy = energy_force!(positions, forces, params, pot)
-    println("First energy ", total_energy, typeof(total_energy))
+    println("First energy ", total_energy)
 
     # Allocate information for random values
-    (_, rng_list) = _create_rngs(3; seed = s.params.seed)
+    (_, rng_list) = _create_rngs(s.dims; seed = s.params.seed)
     random_matrix = zeros(typeof(s.boxl), size(s.system.positions))
 
     # * Main loop
     for i = 1:N
         # Always re-compute random numbers
         rng_matrix!(random_matrix, rng_list)
+        # Move particles and update positions
         ermakv!(positions, forces, s.params.τ, s.boxl, random_matrix)
+        # Re-compute the energy and forces from the system
         total_energy = energy_force!(positions, forces, params, pot)
         if i % 1000 == 0
             @show total_energy
         end
     end
+    # Save all relevant values
     s.energy = total_energy
+    @pack! s.system = positions, forces
 end
 
 function move!(N::Integer, s::SimulationSystem, pot::PairwisePotential)
