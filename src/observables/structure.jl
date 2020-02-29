881ad7d99af57f4d5418c528984b74a016ddde6c
edwinb-ai <developeredwin@gmail.com>
2020-02-28 18:13:27 -0600

    Function argument typing and code structure review

--- src/observables/structure.jl
+++ src/observables/structure.jl
@@ -1,22 +1,31 @@
-function _fourier_structure!(gofr, rdfpos, sqobj, dr, dq, nm)
+function _fourier_structure!(
+    gofr::AbstractArray,
+    rdfpos::AbstractArray,
+    sqobj::StructureFactor,
+    dr::AbstractFloat,
+)
     sum_total = 0.0
 
     for i = 1:sqobj.nm
-        sqobj.sq[i, 1] = (i - 1.0) * dq
+        sqobj.sq[i, 1] = (i - 1.0) * sqobj.dq
     end
 
     for i = 2:sqobj.nm
         sum_total = sum(
             rdfpos .^ 2 .* gofr .* sin.(sqobj.sq[i, 1] * rdfpos) ./
-                (sqobj.sq[i, 1] * rdfpos)
+                (sqobj.sq[i, 1] * rdfpos),
         )
         sum_total *= sqobj.norm_const * dr
         sqobj.sq[i, 2] = 1.0 + sum_total
     end
 end
 
-function compute_structure!(sqobj, gofr, s)
+function compute_structure!(
+    sqobj::StructureFactor,
+    gofr::PairDistributionFunction,
+    s::SimulationSystem,
+)
     rdfpos = @view gofr.gofr[2:end, 1]
     hfunc = gofr.gofr[2:end, 2] .- 1.0
-    _fourier_structure!(hfunc, rdfpos, sqobj, gofr.dr, sqobj.dq, sqobj.nm)
+    _fourier_structure!(hfunc, rdfpos, sqobj, gofr.dr)
 end
